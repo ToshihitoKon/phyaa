237d9dac07a71736e746620427d1ad51f6cdf6d3
Toshihito Kon <toshihito.kon@gmail.com>
2020-08-24 04:39:46 +0900

    insertとselectできた

---
+++ src/main.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+from pathlib import Path
+import pymysql.cursors
+
+music_suffixes = ['.mp3', '.aac', '.flac', '.m4a']
+
+def search_entry(path):
+    paths = []
+    if path.is_dir():
+        for child in path.iterdir():
+            paths.extend(search_entry(child))
+        return paths
+    if path.is_file():
+        if path.suffix in music_suffixes:
+            return [path]
+    return []
+
+def mysql_sync_entry(paths):
+    conn = pymysql.connect(host='172.17.0.1',
+            user='root',
+            db='phyaa_dev',
+            charset='utf8mb4',
+            autocommit=True,
+            cursorclass=pymysql.cursors.DictCursor)
+
+    strpaths = []
+    for path in paths:
+        strpaths.append([str(path)])
+
+    query = 'INSERT INTO paths (hoge) VALUES (%s)'
+    try:
+        with conn.cursor() as cursor:
+            print(cursor.executemany(query, strpaths))
+            print(cursor.execute('select * from paths'))
+            print(cursor.fetchall())
+            #print(cursor.execute('drop table paths'))
+    finally:
+        conn.close()
+
+if __name__ == '__main__':
+    paths = search_entry(Path('.'))
+    mysql_sync_entry(paths)
---
+++ tabeldef.sql
@@ -0,0 +1,3 @@
+create database phyaa_dev;
+use phyaa_dev;
+create table paths (id mediumint not null auto_increment, hoge varchar(192), primary key (id));
