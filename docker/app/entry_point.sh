294158130abc7a46b44f6b718d20a963f5eaac99
Toshihito Kon <toshihito.kon@gmail.com>
2021-03-03 23:43:46 +0900

    add sample music files

--- docker/app/entry_point.sh
+++ docker/app/entry_point.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
 
-python3 -m pip install pymysql
+python3 -m pip install pymysql[rsa]
+
+# dbが立ち上がるまで雑に待つ
+sleep 20
 python3 /usr/src/app/src/main.py
--- src/main.py
+++ src/main.py
@@ -6,7 +6,6 @@ music_suffixes = ['.mp3', '.aac', '.flac', '.m4a']
 
 def search_entry(path):
     paths = []
-    print(path)
     if path.is_dir():
         for child in path.iterdir():
             paths.extend(search_entry(child))
@@ -17,8 +16,10 @@ def search_entry(path):
     return []
 
 def mysql_sync_entry(paths):
-    conn = pymysql.connect(host='db',
+    conn = pymysql.connect(
+            host='db',
             user='root',
+            password='password',
             db='phyaa_dev',
             charset='utf8mb4',
             autocommit=True,
@@ -28,11 +29,11 @@ def mysql_sync_entry(paths):
     for path in paths:
         strpaths.append([str(path)])
 
-    query = 'INSERT INTO paths (hoge) VALUES (%s)'
+    query = 'INSERT INTO files (filepath) VALUES (%s)'
     try:
         with conn.cursor() as cursor:
             print(cursor.executemany(query, strpaths))
-            print(cursor.execute('select * from paths'))
+            print(cursor.execute('select * from files'))
             print(cursor.fetchall())
             #print(cursor.execute('drop table paths'))
     finally:
@@ -40,5 +41,4 @@ def mysql_sync_entry(paths):
 
 if __name__ == '__main__':
     paths = search_entry(Path('.'))
-    print(paths)
-    #mysql_sync_entry(paths)
+    mysql_sync_entry(paths)
